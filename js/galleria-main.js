9e21dddb79b7bbd90e4e18895bdc8cb0894552da
Lucas <lucas.marot@etu-webschoolfactory.fr>
2014-03-21 13:51:15 +0100

    tp

--- js/galleria-main.js
+++ js/galleria-main.js
@@ -1,8 +1,8 @@
-/*// Load the classic theme
-Galleria.loadTheme(GALLERIA_URL . 'lib/galleria/theme/classic/galleria.classic.min.js');*/
-
 // Load the classic theme
-Galleria.loadTheme('http://localhost:8888/Wordpress-2T501/wp-content/plugins/galleria/lib/galleria/theme/classic/galleria.classic.min.js');
+Galleria.loadTheme(GALLERIA_URL . 'lib/galleria/theme/classic/galleria.classic.min.js');
+
+/*// Load the classic theme
+Galleria.loadTheme('http://localhost:8888/Wordpress-2T501/wp-content/plugins/galleria/lib/galleria/theme/classic/galleria.classic.min.js');*/
 
 // Initialize Galleria
 Galleria.run('.galleria');
