4e38013490da643e8d1fd4b9b78068060366462a
BROU <jphi225@live.fr>
2014-03-21 10:21:30 +0100

    Delete galleria-main.js

--- galleria-main.js
+++
@@ -1,8 +0,0 @@
-// insérez le code suivant à la place de l'ancien dans le fichier galleria-main.js
-// emplacement Galleria-master/js/
-// Load the classic theme
-// Galleria.loadTheme('galleria.classic.min.js'); old
-Galleria.loadTheme(WSF_PLUGALLERIA_URL . 'lib/galleria/theme/classic/galleria.classic.min.js'); // new
-
-// Initialize Galleria
-Galleria.run('#galleria');
